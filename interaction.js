ea1168b3d52124da65635c28bbafb213ac8102e2
Gundu Pooja <gundupooja1995@gmail.com>
2020-03-06 14:04:51 -0600

    updated contact.html

--- interaction.js
+++ interaction.js
@@ -2,3 +2,17 @@ function weightConverter(valNum) {
     document.getElementById("outputKilograms").innerHTML = valNum / 2.2046;
 
 }
+function convert() {
+    const mi = document.getElementById("milesLBL").value;
+    const result = (mi * 1.609344) + " km"
+    document.getElementById("kilometersLBL").innerHTML = result;
+    
+    //localstorage to store number of times the converter was used
+    if (localStorage.counter) {
+        localStorage.counter = Number(localStorage.counter) + 1;
+    }
+    else {
+        localStorage.counter = 1;
+    }
+    document.getElementById("numOfConverts").innerHTML = localStorage.counter;
+}
